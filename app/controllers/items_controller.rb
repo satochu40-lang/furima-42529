b4c74adbbc7e2e3ca68681135ad61e38d0c4f185
satochu40-lang <satochu.40@gmail.com>
2025-12-17 18:29:45 +0900

    画像投稿: 商画像のプレビュー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
+     before_action :authenticate_user!, only: [:new, :create] 
+
   def index
       @item = Item.order("created_at DESC")
   end
-
+     
  
   def new
      @item = Item.new 
