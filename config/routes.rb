7df729ef05c3abf5643afa14cd44d7102bfa4d1a
satochu40-lang <satochu.40@gmail.com>
2025-12-21 21:24:05 +0900

    出品商品削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do 
   devise_for :users
   root to: "items#index" 
-   resources :items, only: [:new, :create]
+   resources :items, only: [:index, :new, :create, :show]
    
   # items/index へのルーティングは必要なければ削除しても構いません
   # get 'items/index'
