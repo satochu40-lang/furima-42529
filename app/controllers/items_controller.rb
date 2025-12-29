cccf638e03c575e52793e3fa8ba83e7b73678a79
satochu40-lang <satochu.40@gmail.com>
2025-12-29 18:16:31 +0900

    set_itemを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!, except: [:index,:show] 
     before_action :set_item, only: [:show, :edit, :update]
-    before_action :move_to_index, only: [:show,:edit, :update]
+    before_action :move_to_index, only: [:edit, :update]
   def index
      
      @items = Item.order("created_at DESC")
@@ -48,10 +48,10 @@ class ItemsController < ApplicationController
   
   private
     def set_item
-      
+      @item = Item.find(params[:id])
     end
     def move_to_index
-     @item = Item.find(params[:id])
+    
 
      if @item.user_id != current_user.id
       redirect_to root_path
