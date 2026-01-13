d865f86e40a4033223c418651f079e3f4ed887af
satochu40-lang <satochu.40@gmail.com>
2026-01-14 00:05:10 +0900

    items_controller 修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # すでに作成済みの機能(new, createなど)を維持しつつ、
   # その中に購入機能(orders)をネストさせます
-  resources :items, only: [:new, :create, :show, :edit, :update] do
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :orders, only: [:index, :create]
   end
 end
